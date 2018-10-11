23575ed78d4ee7c3553f7fccecf4666c2d5139fa
peter <p.meulmeester@gmail.com>
2018-10-11 22:20:14 +0200

    added anonymous test file

--- src/eifile.test.js
+++ src/eifile.test.js
@@ -6,36 +6,27 @@ debug("eifile.test");
 let assert = require("assert");
 let load = require("./eifile");
 
+const fileName = "./testdata/test1.dat";
+
 describe("load file", async function() {
   it("file should load", async function() {
     const dataDef = require("./standards/PM304-3.2.js");
-    const lines = await load.load(
-      "../EiCooker-data/data/VECOZO Declaration No. 43 for Logopedie Sterk - 201810040829.dat",
-      dataDef
-    );
+    const lines = await load.load(fileName, dataDef);
 
-    assert.equal(lines.length, 28);
+    assert.equal(lines.length, 4);
   });
 
   it("parse Voorlooprecord", async function() {
     const dataDef = require("./standards/PM304-3.2.js");
-    const lines = await load.load(
-      "../EiCooker-data/data/VECOZO Declaration No. 43 for Logopedie Sterk - 201810040829.dat",
-      dataDef
-    );
+    const lines = await load.load(fileName, dataDef);
     debug(`lines[0] = ${lines[0].substring(0, 2)}`);
 
     const fixyData = load.parseRecord(lines[0], dataDef, "01");
 
     debug(`fixyData[0].Kenmerk record = ${fixyData[0]["Kenmerk record"]}`);
 
-    assert.equal(fixyData[0].Praktijkcode, "05098615");
+    assert.equal(fixyData[0].Praktijkcode, "01234567");
   });
-
-  const fileName =
-    "../EiCooker-data/data/VECOZO Declaration No. 43 for Logopedie Sterk - 201810040829.dat";
-  // const fileName =
-  // "../EiCooker-data/data/VECOZO Declaration No. 28 for Logopedie Sterk - 201810090958.dat";
 
   it("load and write a complete file into mem, json format", async function() {
     const dataDef = require("./standards/PM304-3.2.js");
